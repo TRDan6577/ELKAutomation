1f1fee1258ebbeef28327b522a442da667ebbaed
trd6577 <trd6577@g.rit.edu>
2018-01-03 23:46:03 -0500

    Added certs. Added option idea. Added readme stuff

--- README.md
+++ README.md
@@ -9,7 +9,14 @@ an installation. The whole process was like fighting a hydra; every time I solve
 issue, 2 more issues came up. While many other people may not have had as much of
 a struggling installing the stack as I have, I figure that these scripts could still
 be useful for people that are laz - I mean efficient. It could also be beneficial
-to those setting up a distributed ELK environment
+to those setting up a distributed ELK environment.
+
+It's important to note that while the configuration files contain some options,
+there is no future plan for them to contain all configurable options. If the
+configuration file doesn't contain an option that you think it shouuld,
+please send a pull request. The idea with the configuration files is to
+keep it simple. Give options that you think may be used by many people - not
+options that are only used in very specific cirumstances.
 
 ## Installation
 `git clone https://github.com/trdan6577/elkautomation.git`
@@ -19,3 +26,8 @@ to those setting up a distributed ELK environment
 Place the automation.conf file and the serversetup.sh file onto the server you want
 to host the ELK stack. Make sure the configurations in the automation.conf file are
 what you want and then run the serversetup.sh file
+
+## Things TODO
+* Create a diagram of final product after scripts are done. Include explaination
+* Add a way to restrict access to the private keys so only root and a service
+can access them
--- serverSetup.sh
+++ serverSetup.sh
@@ -8,13 +8,26 @@ SUCCESS="\033[0;32m" # Green
 WARNING="\033[1;33m" # Yellow
 NC="\033[0m"         # No color
 
+CERT_DIR=/etc/pki/elk
+
 # You must be root to run this script
 if [ $(id -u) != 0 ]; then
     echo -e "${ERROR}[-] Error: You must be root to run this script${NC}"
     exit 1
 fi
 
-# Function definitions
+# Make sure that the user edited server_root.conf and v3.ext
+if [[ $(sha256sum server_root.conf) == "657f509b06782c95ab44a4e46675139cdb77163402e1e578def076e1c446c328  server_root.conf" ]]; then
+    echo -e "${ERROR}[-] Error: You must fill in information in the server_root.conf file to successfully create certificates"
+    exit 1
+fi
+
+if [[ $(sha256sum v3.ext) == "a0a5335daa6295596852a1758f0c1b3493dd25ffe81c0a44e59b08df97a410a5  v3.ext" ]]; then
+    echo -e "${ERROR}[-] Error: You must fill in information in the v3.ext file to successfully create certificates"
+    exit 1
+fi
+
+############################ Function definitions #############################
 
 ############################################################
 # Function: update_repos
@@ -51,6 +64,55 @@ check_error(){
 fi
 }
 
+############################################################
+# Function: generate_certs
+# Purpose:  Generates the root, logstash, and kibana certificates. The 
+#           certificates generated follow the requirements for Chrome/Chromium
+#           so they won't give that angry untrusted page in a web browser.
+#           The directory structure is as follows:
+#               $ROOT_DIR/
+#                 |-- certs/
+#                 |-- private/
+#                 |-- logstash/
+#                 |   |-- certs/
+#                 |   |-- private/
+#                 |-- kibana/
+#                 |   |-- certs/
+#                 |   |-- private/
+# Arguments:
+#  $1 - root directory of the certificates. The default is /etc/pki/elk/
+# Returns:
+#  None
+############################################################
+generate_certs() {
+    echo "[*] Generating certificates..."
+
+    # Create the necessary folders
+    mkdir -p /etc/pki/elk/logstash/certs 
+    mkdir $CERT_DIR/logstash/private
+    mkdir -p $CERT_DIR/certs
+    mkdir $CERT_DIR/private
+
+    # Generate the root certificate and key
+    # TODO: Error check
+    openssl genrsa -out /etc/pki/elk/private/server_root.key 4092
+    echo -e "${SUCCESS}[+] Generated root key in $CERT_DIR/private/${NC}"
+    openssl req -x509 -new -nodes -key /etc/pki/elk/private/server_root.key -sha256 -days 3650 -out /etc/pki/elk/certs/server_root.pem
+    echo -e "${SUCCESS}[+] Generated root certificate in $CERT_DIR/certs/${NC}"
+
+    # Generate the Logstash certificate and key
+    openssl req -new -sha256 -nodes -out $CERT_DIR/logstash/private/logstash.csr -newkey rsa:4092 -keyout $CERT_DIR/logstash/private/logstash.key -config <( cat server_root.conf )
+    echo -e "${SUCCESS}[+] Generated logstash key in $CERT_DIR/logstash/private/${NC}"
+    openssl x509 -req -in $CERT_DIR/logstash/private/logstash.csr -CA $CERT_DIR/certs/server_root.pem -CAkey $CERT_DIR/private/server_root.key -CAcreateserial -out $CERT_DIR/logstash/certs/logstash.crt -days 3650 -sha256 -extfile v3.ext
+    echo -e "${SUCCESS}[+] Generated logstash cert in $CERT_DIR/logstash/certs/${NC}"
+
+    # Generate the Kibana certificate and key
+    openssl req -new -sha256 -nodes -out $CERT_DIR/kibana/private/kibana.csr -newkey rsa:4092 -keyout $CERT_DIR/kibana/private/kibana.key -config <( cat server_root.conf )
+    echo -e "${SUCCESS}[+] Generated kibana key in $CERT_DIR/kibana/private/${NC}"
+    openssl x509 -req -in $CERT_DIR/kibana/private/kibana.csr -CA $CERT_DIR/certs/server_root.pem -CAkey $CERT_DIR/private/server_root.key -CAcreateserial -out $CERT_DIR/kibana/certs/kibana.crt -days 3650 -sha256 -extfile v3.ext
+    echo -e "${SUCCESS}[+] Generated kibana cert in $CERT_DIR/kibana/certs/${NC}"
+}
+
 # Make sure the repositories are up to date
 update_repos
 
@@ -75,3 +137,7 @@ update_repos
 echo -n "[*] Installing elasticsearch, logstash, kibana, and apt-transport-https... "
 apt-get -y -q2 install apt-transport-https elasticsearch logstash kibana > /dev/null 2>&1
 check_error "attempting to install the packages" "apt-get install" $?
+
+# Generate certificates. The certificate generation done here satisfies the
+# strict Chrome/Chromium requirements for self-signed certs
+generate_certs
